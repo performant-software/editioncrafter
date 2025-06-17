5eca81ebd418cf2a6dedf7b89cfcfffa3bd48adb
ajolipa <rebecca@performantsoftware.com>
2025-06-17 15:28:50 +0000

    Deploying to gh-pages from @ performant-software/editioncrafter@efe52679f37910af5c631d7d8a1ab175efadadff 🚀

---
+++ assets/react-16-4DGj9wPi.js
@@ -0,0 +1,1 @@
+import{a as n}from"./index-BmJ9tJkI.js";var m=async(e,o)=>new Promise(r=>{n.render(e,o,()=>r(null))}),a=e=>{n.unmountComponentAtNode(e)};export{m as renderElement,a as unmountElement};
